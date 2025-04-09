eb7d5a061e07147e9242f723fe47b29566b6fb22
ricardo <rmohamedhoesein3@gmail.com>
2025-04-09 13:42:25 +0200

    Move the code for parsing to a subfolder.

--- parsing/parse.py
+++ parsing/parse.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import pandas as pd
+import os
 from time_parse import time_parse
 from number_parse import approximate_parse, stress_parse, sports_parse, random_number_parse
 
 ignore = [239,244]
 
-df = pd.read_csv("./ODI-2025.csv",sep=";")
+dir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + ".." + os.path.sep
+exit()
+df = pd.read_csv(dir + "ODI-2025.csv",sep=";")
 df = df.drop(ignore)
 df = approximate_parse(df)
 df = stress_parse(df)
 df = sports_parse(df)
 df = random_number_parse(df)
 df = time_parse(df)
-df.to_csv("./ODI-2025-parsed.csv",sep=";")
+df.to_csv(dir + "ODI-2025-parsed.csv",sep=";")

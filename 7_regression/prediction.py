730ccdda49a91def02709cc6f192b0d42402289b
ricardo <rmohamedhoesein3@gmail.com>
2025-04-17 16:45:29 +0200

    Apply PCA to do dimension reduction.

--- 7_regression/prediction.py
+++ 7_regression/prediction.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import PoissonRegressor
+from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import r2_score
@@ -17,13 +17,39 @@ base_features = [
     "Have you taken a course on machine learning?",
     "Have you taken a course on information retrieval?",
     "Have you taken a course on statistics?",
-    "Have you taken a course on databases?"
+    "Have you taken a course on databases?",
+    "What is your gender?",
+    "I have used ChatGPT to help me with some of my study assignments ",
+    "When is your birthday (date)?",
+    "category_goodday_1",
+    "category_goodday_2"
 ]
 encoded = encoder.fit_transform(pd.DataFrame(df[base_features]))
-x = pd.DataFrame(encoded.toarray(), columns=encoder.get_feature_names_out())
+encoded = pd.DataFrame(encoded.toarray(), columns=encoder.get_feature_names_out())
+
+x = df.copy(deep=True)
+ignore = [
+    "Timestamp",
+    "How many students do you estimate there are in the room?",
+    "What is your stress level (0-100)?",
+    "How many hours per week do you do sports (in whole hours)? ",
+    "Give a random number",
+    "Time you went to bed Yesterday",
+    "What makes a good day for you (1)?",
+    "What makes a good day for you (2)?",
+    "Parsed Give a random number",
+    "Parsed What is your stress level (0-100)?"
+]
+ignore.extend(base_features)
+x = x.drop(ignore, axis=1)
+x = x.join(encoded)
 
 y = df["Parsed What is your stress level (0-100)?"]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3, random_state=13319817)
+
+pca = PCA().fit(x_train)
+x_train = pca.transform(x_train)
+x_test = pca.transform(x_test)
 
 def poisson_prediction(x_train, y_train, x_test, y_test):
     param_grid = {
